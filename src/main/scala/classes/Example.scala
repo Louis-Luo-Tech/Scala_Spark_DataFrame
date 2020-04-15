21896647ade63c2ec996f3969ba2c6d9ca06e445
Louis-Luo-Tech <xiangluoapply@gmail.com>
2020-04-15 01:42:09 -0700

    n

--- src/main/scala/classes/Example.scala
+++ src/main/scala/classes/Example.scala
@@ -21,26 +21,28 @@ object Example extends App{
   df.show()
   val zip = udf((xs: Seq[String], ys: Seq[String]) => xs.zip(ys))
 //val df1 = df.withColumn("new",explode(split(zip(col("name"), col("code")),"\\|")))
-  val df_null = df.filter(col("name").isNull || col("code").isNull)
-    .withColumnRenamed("name","newname")
-    .withColumnRenamed("code","newcode")
-  df_null.show()
-  //df_null.show()
+//val df_null = df.filter(col("name").isNull || col("code").isNull)
+//  .withColumn("len",when(size(split(col("name"),"\\|")) >=1,lit(size(split(col("name"),"\\|")))))
+
+//  val df_null = df.filter(col("name").isNull || col("code").isNull)
+//      .withColumn("newname",split(col("name"),"\\|"))
+//      .withColumn("code",split(col("code"),"\\|")(0))
+
+
+//    df_null.show()
+
+
   val df_not_null = df.filter(col("name").isNotNull && col("code").isNotNull)
-  //df_not_null.show()
   df_not_null.show()
-  val df1 = df_not_null.withColumn("newname",split(col("name"), "\\|")).drop("name").withColumn("newcode",split(col("code"), "\\|")).drop("code")
+  val df1 = df_not_null.withColumn("newname",split(col("name"), "\\|")).drop("name")
+    .withColumn("newcode",split(col("code"), "\\|")).drop("code")
+
   df1.show()
-
   val df2 = df1.withColumn("vars", explode(zip(col("newname"), col("newcode"))))
-
-  val df4 = df2.select(col("p2p_flag").alias("aa"),col("tns_flag"))
-  val df3 = df2.select(col("vars._1").alias("new_name"),col("vars._2").alias("new_code"))
-      .drop("dsf","fsdf")
-  //val df3 = df2.withColumn("new_name",))
-  //println(coll)
-  //df3.show()
-  df4.show()
-  //df_null.union(df2).show()
+//
+  val df3 = df2.select(col("p2p_flag"),col("tns_flag"),col("vars._1").alias("new_name"),col("vars._2").alias("new_code"))
+      .filter(col("new_name") =!= "")
+  df3.show()
+//  df_null.union(df2).show()
   Thread.sleep(1000000)
 }
